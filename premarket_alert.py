292ff2a024c09274752295ef6514f467ebea8b82
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-17 20:00:23 -0700

    Update premarket_alert.py

--- premarket_alert.py
+++ premarket_alert.py
@@ -1,12 +1,12 @@
 import time
 import requests
-import os
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-# Telegram Bot Config (loaded from environment variables)
+# Telegram Bot Config
 bot_token = os.environ.get('bot_token')
 chat_id = os.environ.get('chat_id')
 
